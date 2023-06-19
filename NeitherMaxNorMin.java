b7478b754a2c52a1326abac112654f395110225a
Swarga-codes <sd3526@srmist.edu.in>
2023-06-20 00:53:50 +0530

    update neither max nor min

--- NeitherMaxNorMin.java
+++ NeitherMaxNorMin.java
@@ -3,29 +3,24 @@ public class NeitherMaxNorMin {
         int[] nums={2,4,24};
         System.out.println(findNonMinOrMax(nums));
     }
-    public static int findNonMinOrMax(int[] nums) {
+     public static int findNonMinOrMax(int[] nums) {
         if(nums.length<=2){
             return -1;
         }
         for(int i=0;i<nums.length;i++){
-            if(nums[i]!=max(nums) && nums[i]!=min(nums)){
+            if(nums[i]!=maxAndmin(nums)[0] && nums[i]!=maxAndmin(nums)[1]){
                 return nums[i];
             }
         }
         return -1;
     }
-    public static int max(int[] nums){
+    public static int[] maxAndmin(int[] nums){
         int max=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            max=Math.max(max,nums[i]);
-        }
-        return max;
-    }
-     public static int min(int[] nums){
         int min=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++){
-            min=Math.min(min,nums[i]);
+            max=Math.max(max,nums[i]);
+             min=Math.min(min,nums[i]);
         }
-        return min;
+        return new int[]{max,min};
     }
 }
