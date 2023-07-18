02c7676c827f96cd2fcbf89e1cb30af1126e7bb7
Swarga-codes <sd3526@srmist.edu.in>
2023-07-19 03:05:46 +0530

    added optimsed solution for reverse vowels of a string

--- ReverseVowelsOfAString.java
+++ ReverseVowelsOfAString.java
@@ -1,28 +1,32 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-
 public class ReverseVowelsOfAString {
     public static void main(String[] args) {
         String s="leetcode";
         System.out.println(reverseVowels(s));
     }
     public static String reverseVowels(String s) {
-        String tmp="";
-        List<String> list=Arrays.asList("a","e","i","o","u","A","E","I","O","U");
-        HashSet<String> set=new HashSet<>();
-        set.addAll(list);
-        for(int i=0;i<s.length();i++){
-                if(set.contains(s.charAt(i)+"")){
-                tmp+=s.charAt(i);
+        char[] ch = s.toCharArray();
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            while (i < j && !isVowel(ch[i])) {
+                i++;
+            }
+            while (i < j && !isVowel(ch[j])) {
+                j--;
+            }
+            if (i < j) {
+                char tmp = ch[i];
+                ch[i] = ch[j];
+                ch[j] = tmp;
+                i++;
+                j--;
             }
         }
-        int k=tmp.length()-1;
-        for(int i=0;i<s.length();i++){
-                if(set.contains(s.charAt(i)+"")){
-                 s=s.substring(0,i)+tmp.charAt(k--)+s.substring(i+1);
-        }
+        return String.valueOf(ch);
     }
-    return s;
-}
+    
+    public static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+               ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U';
+    }
+    
 }
