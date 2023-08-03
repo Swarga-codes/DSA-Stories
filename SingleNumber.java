9d4ca1f663112c1d94ab3c9c903b4256f188a4dc
Swarga-codes <sd3526@srmist.edu.in>
2023-08-03 22:33:24 +0530

    added updated code for single number

--- SingleNumber.java
+++ SingleNumber.java
@@ -4,29 +4,22 @@ public class SingleNumber {
         int[] arr={1,1,2,3,3,4,4};
         System.out.println(singleNumber(arr));
     }
-    public static int singleNumber(int[] arr) {
-        int k=1,pos=0;
-        if(arr.length==1){
-            return arr[0];
-        }
-        Arrays.sort(arr);
-        for(int i=0;i<arr.length-1;i++){
-            if(i+1==arr.length-1 && arr[i+1]!=arr[i]){
-                return arr[i+1];
+    public static int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
             }
-            if(arr[i]==arr[i+1]){
-                k++;
-            }
-            else
-            {
-                if(k==1){
-                    pos=arr[i];
-                }
-                else{
-                    k=1;
-                }
+            else{
+                map.put(nums[i],1);
             }
         }
-        return pos;
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                return entry.getKey();
+            }
         }
+        return -1;
+    }
 }
