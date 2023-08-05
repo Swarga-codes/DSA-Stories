ab96d6cfff8852e94cfb56bdf04483aee7a4590d
Prajjawal Sharma <meprajjwalsharma@gmail.com>
2023-08-06 02:07:52 +0530

    added Reverse Integer Leetcode Q7

---
+++ ReverseInteger.java
@@ -0,0 +1,15 @@
+public class ReverseInteger {
+        public int reverse(int x) {
+            int rev=0;
+            while(x!=0){
+                int digit= x%10;
+                if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10){
+                    return 0;
+                }
+                rev=rev*10 + digit;
+                x=x/10;
+            } 
+            
+                return rev;
+        }
+}
