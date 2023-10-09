f97903bc196dc598e7c42bda4a989c44dd041067
Swarga-codes <sd3526@srmist.edu.in>
2023-10-09 17:39:49 +0530

    updated code for first last position in sorted array

--- FirstLastPositionInSortedArray.java
+++ FirstLastPositionInSortedArray.java
@@ -6,33 +6,18 @@ public class FirstLastPositionInSortedArray {
         System.out.println(Arrays.toString(searchRange(nums,6)));
     }
     public static int[] searchRange(int[] nums, int target) {
-        int[] res=new int[2];
-        int i,j;
-        if(nums.length==0){
-            return new int[]{-1,-1};
-        }
-        if(nums.length==1 && nums[0]==target){
-            return new int[]{0,0};
-        }
-        for(i=0;i<nums.length;i++){
-            if(nums[i]==target){
-               
-                res[0]=i;
-                break;
+        int i=0,j=nums.length-1;
+        while(i<=j){
+            if(nums[i]==target && nums[j]==target){
+                return new int[]{i,j};
+            }
+            else if(nums[i]!=target){
+                i++;
+            }
+            else {
+                j--;
             }
         }
-        for(j=nums.length-1;j>=0;j--){
-            if(nums[j]==target){
-                res[1]=j;
-                break;
-            }
-        }
-        if(i==j){
-            return new int[]{i,i};
-        }
-        if(res[0]==0 && res[1]==0){
-            return new int[]{-1,-1};
-        }
-        return res;
+        return new int[]{-1,-1};
     }
 }
