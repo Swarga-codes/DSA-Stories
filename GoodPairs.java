7af50585d63b295fb34e35109df12f2fd3058d4c
Swarga-codes <sd3526@srmist.edu.in>
2023-10-15 03:48:34 +0530

    optimised number of good pairs

--- GoodPairs.java
+++ GoodPairs.java
@@ -1,17 +1,21 @@
+import java.util.*;
 public class GoodPairs {
     public static void main(String[] args) {
         int[] nums={1,2,3,1,1,3};
         System.out.println(numIdenticalPairs(nums));
     }
     public static int numIdenticalPairs(int[] nums) {
-        int k=0;
+        int c=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            for(int j=1;j<nums.length;j++){
-                if(nums[i]==nums[j] && i<j){
-                    k++;
-                }
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+                c+=map.get(nums[i]);
+            }
+            else{
+                map.put(nums[i],0);
             }
         }
-        return k;
+         return c;
     }
 }
