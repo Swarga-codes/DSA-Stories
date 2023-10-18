8e34bcef10be4980df681d012299835d6f38c07c
Swarga-codes <sd3526@srmist.edu.in>
2023-10-18 21:26:52 +0530

    added two sum 2 - input array is sorted

---
+++ TwoSum2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class TwoSum2 {
+    public static void main(String[] args) {
+        int[] numbers = {2,7,11,15}; int target = 9;
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+    public static int[] twoSum(int[] numbers, int target) {
+        int i=0,j=numbers.length-1;
+        while(i<=j){
+            if(numbers[i]+numbers[j]==target){
+                return new int[]{i+1,j+1};
+            }
+            else if(numbers[i]+numbers[j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
