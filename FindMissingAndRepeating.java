1f4b9e14d057de5b3bd7c36cc34f6601b1315b0c
Swarga-codes <sd3526@srmist.edu.in>
2024-06-06 01:38:06 +0530

    updated find missing and repeating solution

--- FindMissingAndRepeating.java
+++ FindMissingAndRepeating.java
@@ -6,24 +6,21 @@ public class FindMissingAndRepeating {
         System.out.println(Arrays.toString(findTwoElement(arr, n)));
     }
     public static int[] findTwoElement(int arr[], int n) {
-        int[] res = new int[2];
-        boolean flag=false;
-        HashSet<Integer> set=new HashSet<>();
+        int[] res=new int[2];
+          Set<Integer> set=new HashSet<>();
         for(int i=0;i<n;i++){
-            if(!flag && set.contains(arr[i])){
+            if(set.contains(arr[i])){
                 res[0]=arr[i];
-                flag=true;
             }
-            set.add(arr[i]);
+           set.add(arr[i]);
+           
         }
-        
-    
-       for(int i=1;i<=n;i++){
-           if(!set.contains(i)){
-               res[1]=i;
-               break;
-           }
-       }
+        for(int i=1;i<=n;i++){
+            if(!set.contains(i)) {
+                res[1]=i;
+                break;
+            }
+        }
         return res;
     }
 }
