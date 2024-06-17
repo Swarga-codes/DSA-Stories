c7324a5a13a12e878473c3a631ba96d8e1732fc3
Swarga-codes <sd3526@srmist.edu.in>
2024-06-18 03:42:49 +0530

    updated single element in a sorted array solution

--- SingleElementInASortedArray.java
+++ SingleElementInASortedArray.java
@@ -4,21 +4,25 @@ public class SingleElementInASortedArray {
         System.out.println(singleNonDuplicate(arr));
     }
     public static int singleNonDuplicate(int[] nums) {
-        if(nums.length==1){
-            return nums[0];
+        int l=0,h=nums.length-1,mid;
+        if(nums.length==1) return nums[0];
+        if(nums[0]!=nums[1]) return nums[0];
+        if(nums[h]!=nums[h-1]) return nums[h];
+        l=1;
+        h=nums.length-2;
+        while(l<=h){
+            mid=(l+h)/2;
+            if(nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]){
+                return nums[mid];
+            }
+            else if(mid%2==0 && nums[mid]==nums[mid+1] || mid%2!=0 && nums[mid]==nums[mid-1]){
+                l=mid+1;
+            }
+            else{
+                h=mid-1;
+            }
         }
-        if(nums[0]!=nums[1]){
-            return nums[0];
-        }
-        if(nums[nums.length-1]!=nums[nums.length-2]){
-            return nums[nums.length-1];
-        }
-       for(int i=0;i<nums.length-1;i++){
-        if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
-            return nums[i];
-        }
-          
-       }
-       return 0;
+        return -1;
+      
     }
 }
