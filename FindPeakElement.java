5b430b4b0f995d5b48af03cc215b365736f4ebd1
Swarga-codes <sd3526@srmist.edu.in>
2024-06-18 19:56:45 +0530

    updated find peak element solution

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -4,20 +4,23 @@ public class FindPeakElement {
         System.out.println(findPeakElement(nums));
     }
     public static int findPeakElement(int[] nums) {
-        for(int i=1;i<nums.length-1;i++){
-            if(nums[i]>nums[i+1] && nums[i]>nums[i-1]){
-                return i;
+        if(nums.length==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
+        int l=1,h=nums.length-2,mid;
+        while(l<=h){
+            mid=(l+h)/2;
+            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+              return mid;
+                
+            }
+            else if(nums[mid]<nums[mid+1]){
+                l=mid+1;
+            }
+            else if(nums[mid]>nums[mid+1]){
+                h=mid-1;
             }
         }
-        return maxi(nums);
-    }
-    public static int maxi(int[] nums){
-        int pos=0;
-        for(int i=1;i<nums.length;i++){
-            if(nums[pos]<nums[i]){
-                pos=i;
-            }
-        }
-        return pos;
+        return -1;
     }
 }
