fade6c11201be7c5a38810b60de78af2097e7448
Swarga-codes <sd3526@srmist.edu.in>
2024-12-11 19:31:38 +0530

    added updated solution for add two numbers

--- LinkedList/AddTwoNumbers.java
+++ LinkedList/AddTwoNumbers.java
@@ -7,54 +7,37 @@ public class AddTwoNumbers {
      ListNode(int val) { this.val = val; }
      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  }
-     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1==null && l2==null){
-            return null;
-        }
-        if(l1==null){
-            return l2;
-        }
-        if(l2==null){
-            return l1;
-        }
-        ListNode tmp=new ListNode(0);
-        ListNode res=tmp;
-       int carry=0;
-       while(l1!=null && l2!=null){
-           int sum=l1.val+l2.val+carry;
-           carry=0;
-           if(sum>9){
-               carry=sum/10;
-           }
-            tmp.next=new ListNode(sum%10);
-            tmp=tmp.next;
-            l1=l1.next;
-            l2=l2.next;
-       }
-       while(l1!=null){
-           int sum=l1.val+carry;
-           carry=0;
-            if(sum>9){
-               carry=sum/10;
-           }
-             tmp.next=new ListNode(sum%10);
-            tmp=tmp.next;
-                  l1=l1.next;
-       }
-         while(l2!=null){
-           int sum=l2.val+carry;
-           carry=0;
-            if(sum>9){
-               carry=sum/10;
-           }
-             tmp.next=new ListNode(sum%10);
-            tmp=tmp.next;
-                  l2=l2.next;
-       }
-       if(carry!=0){
-           tmp.next=new ListNode(carry);
-           tmp=tmp.next;
-       }
-       return res.next;
+ public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    ListNode tmp1=l1;
+    ListNode tmp2=l2;
+    ListNode res=new ListNode(0);
+    ListNode tmp3=res;
+    int sum=0,carry=0;
+    while(tmp1!=null && tmp2!=null){
+        sum=tmp1.val+tmp2.val+carry;
+        carry=sum/10;
+        tmp3.next=new ListNode(sum%10);
+        tmp3=tmp3.next;
+        tmp1=tmp1.next;
+        tmp2=tmp2.next;
     }
+    while(tmp1!=null){
+        sum=tmp1.val+carry;
+        carry=sum/10;
+        tmp3.next=new ListNode(sum%10);
+        tmp3=tmp3.next;
+        tmp1=tmp1.next;
+    }
+     while(tmp2!=null){
+        sum=tmp2.val+carry;
+        carry=sum/10;
+        tmp3.next=new ListNode(sum%10);
+        tmp3=tmp3.next;
+        tmp2=tmp2.next;
+    }
+    if(carry!=0){
+        tmp3.next=new ListNode(carry);
+    }
+    return res.next;
+}
 }
