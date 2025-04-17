3fc5828034fc7193f59eff8996121adc7e59c1ff
Swarga-codes <sd3526@srmist.edu.in>
2025-04-18 03:17:00 +0530

    Added count number of nice subarrays

--- CountNumberOfNiceSubarrays.java
+++ CountNumberOfNiceSubarrays.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class CountNumberOfNiceSubarrays {
     public static void main(String[] args) {
@@ -7,19 +6,24 @@ public class CountNumberOfNiceSubarrays {
         int k = 3;
         System.out.println(numberOfSubarrays(nums, k));
     }
-       public static int numberOfSubarrays(int[] nums, int k) {
-        int count=0,ans=0;
-        Map<Integer,Integer> map=new HashMap<>();
-        map.put(0,1);
+    public static int numberOfSubarrays(int[] nums, int k) {
         for(int i=0;i<nums.length;i++){
-           if(nums[i]%2!=0){
-            count++;
-           }
-            if(map.containsKey(count-k)){
-                ans+=map.get(count-k);
-            }
-            map.put(count,map.getOrDefault(count,0)+1);
+            if(nums[i]%2!=0) nums[i]=1;
+            else nums[i]=0;
         }
-        return ans;
+        return helper(nums,k)-helper(nums,k-1);
+    }
+    public static int helper(int[] nums,int k){
+        int r=0,l=0,res=0,sum=0;
+        while(r<nums.length){
+            sum+=nums[r]%2;
+            while(sum>k){
+                sum-=nums[l]%2;
+                l++;
+            }
+            res+=(r-l+1);
+            r++;
+        }
+        return res;
     }
 }
